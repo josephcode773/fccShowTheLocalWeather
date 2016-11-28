3fccd66f593e1a79a27643b346e93a00efdebb33
josephcode773 <josephcode773@gmail.com>
2016-11-28 17:24:52 -0600

    Version 2 Start

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-var lat = 0;
-var lon = 0;
+// Project Requirement #1: I can see the weather in my current location.
+// - Get lat and lon numbers  //done! -Canceled
+// - Use Use openweathermap.org , by geographic coordinates and API for JSON.
+// - Version 2:
+//  - changed API to www.wunderground.com, it meets all requirments.
 
-// function success(pos) {
-//     var crd = pos.coords;
+// var lat = 0;
+// var lon = 0;
 //
+// navigator.geolocation.getCurrentPosition(function (pos) {
 //     lat = pos.coords.latitude;
 //     lon = pos.coords.longitude;
+// });
+//
+// var lattie = function () {
+//     var lat = 0;
+//     navigator.geolocation.getCurrentPosition(function (pos) {
+//         lat = pos.coords.latitude;
+//         // lon = pos.coords.longitude;
+//     });
 // };
 
-navigator.geolocation.getCurrentPosition(function (pos) {
-    lat = pos.coords.latitude;
-    lon = pos.coords.longitude;
+var tweetText = '';
+$(document).ready(function () {
+    $("#newQuote").on('click', function () {
+        $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function (json) {
+            $("#quoteText").html(JSON.stringify(json.quoteText));
+            $("#quoteAuthor").html(JSON.stringify(json.quoteAuthor));
+            tweetText = json.quoteText + json.quoteAuthor;
+            $("a.myLink").attr("href", function () {
+                var textie = '';
+                textie = "https://twitter.com/intent/tweet?text=Quote: "+tweetText;
+                return textie;
+            });
+            $.ajaxSetup({ cache: false});
+        });
+    });
 });
