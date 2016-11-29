7a321497744a747e5c15aa9a49552bca09da8205
josephcode773 <josephcode773@gmail.com>
2016-11-29 01:03:05 -0600

    Version 2: - Make button that alternates between Fahrenheit/Celsius.  /done!

--- script.js
+++ script.js
@@ -36,20 +36,35 @@
 //     });
 // });
 
+var far = "";
+var cel = "";
+var farOut = "";
+var celOut = "";
 $(document).ready(function () {
     $.getJSON("http://api.wunderground.com/api/e3efe06177afa5ad/conditions/q/autoip.json?", function (json) {
-        var far = json.current_observation.temp_f;
-        var cel = json.current_observation.temp_c;
+        far = json.current_observation.temp_f;
+        cel = json.current_observation.temp_c;
+        farOut = (far + ' Degrees '+ 'Fahrenheit');
+        celOut = (cel + ' Degrees '+ 'Celsius');
         $("#displayLocation").html(JSON.stringify(json.current_observation.display_location.full));
-        $("#currentTemp").html(far + '&deg' + 'F');
+        $("#currentTemp").html(far + ' Degrees '+ 'Fahrenheit');
         $("img").attr("src", function () {
             var textie = json.current_observation.icon_url;
             return textie;
         });
-        $("#changeFtoC").on('click', function () {
-            $("#currentTemp").html(cel + '&deg' + 'C');
-        });
+        // $("#changeFtoC").on('click', function () {
+        //     $("#currentTemp").html(cel + '&deg' + 'C');
+        // });
         $.ajaxSetup({ cache: false});
 
     })
 });
+
+function changeText() {
+    var demo = document.getElementById('currentTemp');
+    if (demo.innerHTML === farOut) {
+        demo.innerHTML = celOut;
+    } else {
+        demo.innerHTML = farOut;
+    }
+}
