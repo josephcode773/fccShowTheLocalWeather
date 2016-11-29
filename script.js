94161d1872f0f4300576c1e718f2da231d441674
josephcode773 <josephcode773@gmail.com>
2016-11-28 23:07:13 -0600

    Version 2: - changed API to www.wunderground.com, it meets all requirements.  //done! - Make button that alternates between Fahrenheit/Celsius.  /pending

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 // Project Requirement #1: I can see the weather in my current location.
-// - Get lat and lon numbers  //done! -Canceled
-// - Use Use openweathermap.org , by geographic coordinates and API for JSON.
 // - Version 2:
-//  - changed API to www.wunderground.com, it meets all requirments.
+//  - changed API to www.wunderground.com, it meets all requirements.  //done!
+//  - Make button that alternates between Fahrenheit/Celsius.
 
-// var lat = 0;
-// var lon = 0;
-//
-// navigator.geolocation.getCurrentPosition(function (pos) {
-//     lat = pos.coords.latitude;
-//     lon = pos.coords.longitude;
-// });
-//
-// var lattie = function () {
-//     var lat = 0;
-//     navigator.geolocation.getCurrentPosition(function (pos) {
-//         lat = pos.coords.latitude;
-//         // lon = pos.coords.longitude;
+// var tweetText = '';
+// $(document).ready(function () {
+//     $("#newQuote").on('click', function () {
+//         $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function (json) {
+//             $("#quoteText").html(JSON.stringify(json.quoteText));
+//             $("#quoteAuthor").html(JSON.stringify(json.quoteAuthor));
+//             tweetText = json.quoteText + json.quoteAuthor;
+//             $("a.myLink").attr("href", function () {
+//                 var textie = '';
+//                 textie = "https://twitter.com/intent/tweet?text=Quote: "+tweetText;
+//                 return textie;
+//             });
+//             $.ajaxSetup({ cache: false});
+//         });
 //     });
-// };
+// });
 
-var tweetText = '';
+// $(document).ready(function () {
+//     $("#newQuote").on('click', function () {
+//         $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function (json) {
+//             $("#quoteText").html(JSON.stringify(json.quoteText));
+//             $("#quoteAuthor").html(JSON.stringify(json.quoteAuthor));
+//             tweetText = json.quoteText + json.quoteAuthor;
+//             $("a.myLink").attr("href", function () {
+//                 var textie = '';
+//                 textie = "https://twitter.com/intent/tweet?text=Quote: "+tweetText;
+//                 return textie;
+//             });
+//             $.ajaxSetup({ cache: false});
+//         });
+//     });
+// });
+
 $(document).ready(function () {
-    $("#newQuote").on('click', function () {
-        $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function (json) {
-            $("#quoteText").html(JSON.stringify(json.quoteText));
-            $("#quoteAuthor").html(JSON.stringify(json.quoteAuthor));
-            tweetText = json.quoteText + json.quoteAuthor;
-            $("a.myLink").attr("href", function () {
-                var textie = '';
-                textie = "https://twitter.com/intent/tweet?text=Quote: "+tweetText;
-                return textie;
-            });
-            $.ajaxSetup({ cache: false});
+    $.getJSON("http://api.wunderground.com/api/e3efe06177afa5ad/conditions/q/autoip.json?", function (json) {
+        $("#displayLocation").html(JSON.stringify(json.current_observation.display_location.full));
+        $("#currentTemp").html(JSON.stringify(json.current_observation.temp_f));
+        $("img").attr("src", function () {
+            var textie = json.current_observation.icon_url;
+            return textie;
         });
-    });
+        $("#changeFtoC").on('click', function () {
+
+        });
+        $.ajaxSetup({ cache: false});
+    })
 });
